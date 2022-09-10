68c327610b53ed9b1749bfe46109a65ae6026050
Joao Carlos Azevedo <jcorreadeazevedo@gmail.com>
2022-09-10 09:18:41 -0400

    feat: add ontap to menus and routing

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:nexus_estoque/features/menu/presentantion/pages/menu_page.dart';
 
 class AppRouter {
@@ -6,8 +7,44 @@ class AppRouter {
     switch (settings.name) {
       case "/":
         return MaterialPageRoute(builder: (context) => const MenuPage());
+      case "/enderecas":
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(title: 'Enderecas'));
+      case "/movimentos":
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(title: 'Movimentos'));
+      case "/transferencias":
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(title: 'Transferencias'));
+      case "/consulta":
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(title: 'Consulta'));
+      case "/configuracoes":
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(title: 'Configuracoes'));
       default:
-        return MaterialPageRoute(builder: (context) => const Scaffold());
+        return MaterialPageRoute(
+            builder: (context) => const DefaultPage(
+                  title: 'defaul',
+                ));
     }
+  }
+}
+
+class DefaultPage extends StatelessWidget {
+  final String title;
+
+  const DefaultPage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(title),
+      ),
+    );
   }
 }
--- lib/features/menu/presentantion/pages/menu_page.dart
+++ lib/features/menu/presentantion/pages/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nexus_estoque/core/constants/menus.dart';
 import 'package:nexus_estoque/features/menu/presentantion/pages/widgets/menu_card_widget.dart';
 import 'package:rive/rive.dart';
@@ -16,46 +17,64 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).secondaryHeaderColor,
       appBar: AppBar(
-        title: const Text("Menus"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Expanded(
-              flex: 1,
-              child: Card(
-                elevation: 5,
-                color: Theme.of(context).backgroundColor,
-                child: const Center(
-                  child: RiveAnimation.asset(
-                    fit: BoxFit.scaleDown,
-                    'assets/inventory_app.riv',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Expanded(
-              flex: 3,
-              child: GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: menuItens.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: crossAxisCount,
-                  crossAxisSpacing: 15,
-                  mainAxisSpacing: 15,
-                  //childAspectRatio: childAspectRatio,
-                ),
-                itemBuilder: (context, index) =>
-                    MenuCard(info: menuItens[index]),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/configuracoes');
+              },
+              child: FaIcon(
+                FontAwesomeIcons.gear,
+                color: Theme.of(context).primaryColor,
               ),
             ),
           ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Card(
+                  elevation: 5,
+                  color: Theme.of(context).backgroundColor,
+                  child: const Center(
+                    child: RiveAnimation.asset(
+                      fit: BoxFit.scaleDown,
+                      'assets/inventory_app.riv',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Expanded(
+                flex: 3,
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: menuItens.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 15,
+                    //childAspectRatio: childAspectRatio,
+                  ),
+                  itemBuilder: (context, index) =>
+                      MenuCard(info: menuItens[index]),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
