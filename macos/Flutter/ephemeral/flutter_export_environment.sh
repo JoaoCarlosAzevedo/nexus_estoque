fa55353ae9632c568398f3e1df3e9d73eef26dd8
Joao Carlos <jcorreadeazevedo@gmail.com>
2023-02-12 19:31:30 -0400

    change kotlin jdk from 7 to 8

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Projects\nexus_estoque"
+export "FLUTTER_ROOT=/Users/joaocarlosazevedo/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/joaocarlosazevedo/Documents/projects/nexus_estoque"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
