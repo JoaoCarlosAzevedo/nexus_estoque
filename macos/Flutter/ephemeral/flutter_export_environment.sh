53955dbdc4d86b56c15325b2dac883ab0047db9f
Joao Carlos <jcorreadeazevedo@gmail.com>
2026-05-27 12:53:45 -0400

    ajustes para build

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\joaoc\fvm\versions\3.22.0"
+export "FLUTTER_ROOT=C:\Flutter\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Projects\nexus_estoque"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Projects\nexus_estoque\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.65"
